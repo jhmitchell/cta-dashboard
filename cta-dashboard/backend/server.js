6dd0e1ed42d8a69499ad81b86e6583b2f67ec378
John Mitchell <jhmitchell@uchicago.edu>
2023-05-01 15:50:05 -0500

    Bus positions for a single route

--- cta-dashboard/backend/server.js
+++ cta-dashboard/backend/server.js
@@ -9,5 +9,22 @@ const app = express();
 app.use(cors());
 
 const API_KEY = process.env.CTA_API_KEY;
-const ROUTE = '171';
+const ROUTE = '6';
 
+app.get('/api/getvehicles', async (req, res) => {
+    const url = `http://www.ctabustracker.com/bustime/api/v2/getvehicles?key=${API_KEY}&rt=${ROUTE}&format=json`;
+    console.log('Fetching vehicles from:', url)
+
+    try {
+        const response = await axios.get(url);
+        res.json(response.data);
+    } catch (error) {
+        console.log('Error fetching vehicles:', error);
+        res.status(500).json({ error: 'Error fetching vehicles' }); 
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
--- cta-dashboard/src/App.jsx
+++ cta-dashboard/src/App.jsx
@@ -4,22 +4,43 @@ import axios from 'axios';
 function App() {
   const [buses, setBuses] = useState([]);
 
+  const fetchBuses = async () => {
+    console.log('fetching buses')
+    const url = 'http://localhost:5000/api/getvehicles';
+
+    try {
+      const response = await axios.get(url);
+      const bustimeResponse = response.data['bustime-response']
+
+      // If no buses are found, bustime-response will return an error
+      // instead of an array of vehicles
+      if (bustimeResponse.error) {
+        throw new Error(bustimeResponse.error[0].msg);
+      }
+
+      const data = bustimeResponse.vehicle;
+      setBuses(data);
+    } catch (error) {
+      console.error('Error fetching vehicles:', error);
+    }
+  };
+
   useEffect(() => {
+    console.log('mounting fetchbuses')
     fetchBuses();
   }, []);
-
-  const fetchBuses = async () => {
-    /* TODO */
-  };
 
   return (
     <>
       <h1>CTA Bus Tracker</h1>
-      <div>
+      <h2>Route #6</h2>
+      <ul>
         {buses.map((bus) => (
-          /* TODO */
+          <li key={bus.vid}>
+            Bus #{bus.vid} is at {bus.lat}, {bus.lon}
+          </li>
         ))}
-      </div>
+      </ul>
     </>
   );
 }
